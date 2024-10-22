42d13d36c88f76768d4cf25d2caaf528fdd469a7
doShant-dev <gdoshant@gmail.com>
2024-10-22 15:54:49 +0000

    Minor footer improvements

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -3,54 +3,56 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-<footer className="bg-light text-center text-lg-start">
-
-  <div className="container p-4">
-    <div className="row">
-      <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-        <h5 className="text" style={{fontFamily: "Playwrite GB S, cursive", fontWeight: "bold", fontSize: "32px"}}>FantasticFour</h5>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia aperiam veniam, mollitia, quae iure ipsum sunt sed numquam suscipit placeat, praesentium saepe.
-        </p>
+    <footer className="bg-light text-center text-lg-start" style={{marginTop: '2.5rem'}}>
+      <div className="container p-4">
+        {/* Links */}
+        <div className="row">
+          {/* Column 1 */}
+          <div className="col-lg-4 col-md-12 col-md-12 mb-4 mb-md-0">
+            <h5 className="text" style={{ fontWeight: 'bold', fontFamily: 'Playwrite GB S, cursive', fontSize: '32px'}}>FastasticFour</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde blanditiis inventore explicabo ab nobis sed eius, at laboriosam magni laborum? <br />
+            </p>
+          </div>
+          <span className='col-lg-2' />
+    
+          {/* Column 2 */}
+          <div className="col-lg-2 col-md-6 col-sm-6 col-xs-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Links</h5>
+            <ul className="list-unstyled mb-0">
+              <li><Link to="/" className="text-dark">Home</Link></li>
+              <li><Link to="/about" className="text-dark">About</Link></li>
+              <li><Link to="/contact" className="text-dark">Contact</Link></li>
+            </ul>
+          </div>
+    
+          {/* Column 3 */}
+          <div className="col-lg-2 col-md-6 col-sm-6 col-xs-6 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Follow Us</h5>
+            <ul className="list-unstyled mb-0">
+              <li><a href="#" target='_blank' className="text-dark">Twitter</a></li>
+              <li><a href="#" target='_blank' className="text-dark">Facebook</a></li>
+              <li><a href="#" target='_blank' className="text-dark">Instagram</a></li>
+            </ul>
+          </div>
+    
+          {/* Column 4 */}
+          <div className="col-lg-2 col-md-12 col-sm-12 mb-4 mb-md-0">
+            <h5 className="text-uppercase">Contact Us</h5>
+            <p>
+              123 Zen Street, Peace City, PC 45678<br />
+              Email: contact@zensite.com<br />
+              Phone: +123 456 7890
+            </p>
+          </div>
+        </div>
+        {/* Links end */}
       </div>
-
-      <div>
-
-      <div className="col-lg-2 col-md-4 mb-4 mb-md-0">
-        <h5 className="text-uppercase">Links</h5>
-        <ul className="list-unstyled mb-0">
-          <li><Link to="/" className="text-dark">Home</Link></li>
-          <li><Link to="/contact" className="text-dark">Contact</Link></li>
-          <li><Link to="/about" className="text-dark">About</Link></li>
-        </ul>
+    
+      {/* Copyright */}
+      <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.05)'}}>
+        © 2024 FantasticFour. All rights reserved.
       </div>
-
-      <div className="col-lg-2 col-md-4 mb-4 mb-md-0">
-        <h5 className="text-uppercase mt-1">Follow Us</h5>
-        <ul className="list-unstyled mb-0">
-          <li><a target='_blank' href="#" className="text-dark">Facebook</a></li>
-          <li><a target='_blank' href="#" className="text-dark">Instagram</a></li>
-          <li><a target='_blank' href="#" className="text-dark">Twitter</a></li>
-        </ul>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-        <h5 className="text-uppercase">Contact Us</h5>
-        <p>
-          123 Zen Street, Peace City, PC 45678<br />
-          Email: contact@fantastic4.com<br />
-          Phone: +123 456 7890
-        </p>
-      </div>
-    </div>
-  </div>
-
-  <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
-    © 2024 FantasticFour. All rights reserved.
-  </div>
-  </div>
-</footer>
-  )
-}
-
+    </footer>
+  )}
 export default Footer;
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,74 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-  //   <nav classNameName="navbar navbar-expand-lg bg-body-tertiary" style={style.header}>
-  //   <div classNameName="container-fluid">
-  //     <a classNameName="navbar-brand" style={style.title} href="/">Fantastic Four</a>
-  //     <button classNameName="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  //       <span classNameName="navbar-toggler-icon"></span>
-  //     </button>
-  //     <div classNameName="collapse navbar-collapse" id="navbarSupportedContent">
-  //       <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
-  //         <li classNameName="nav-item">
-  //           <a classNameName="nav-link active" aria-current="page" href="/about">About</a>
-  //         </li>
-  //         <li classNameName="nav-item">
-  //           <a classNameName="nav-link" href="/contact">Contact</a>
-  //         </li>
-  //         <li classNameName="nav-item dropdown">
-  //           <a classNameName="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-  //             Account
-  //           </a>
-  //           <ul classNameName="dropdown-menu">
-  //             <li><a classNameName="dropdown-item" href="/login">Login</a></li>
-  //             <li><a classNameName="dropdown-item" href="#">Logout</a></li>
-  //             <li><a classNameName="dropdown-item" href="signup">Register</a></li>
-  //           </ul>
-  //         </li>
-  //       </ul>
-  //       <form classNameName="d-flex" role="search">
-  //         <input classNameName="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-  //         <button classNameName="btn btn-outline-success" type="submit">Search</button>
-  //       </form>
-  //     </div>
-  //   </div>
-  // </nav> 
-
-
-<nav className="navbar navbar-expand-lg navbar-light">
-    <div className="container">
-      <a className="navbar-brand" style={style.title} href="/">FantasticFour</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/contact">Contact</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/about">About</a>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container">
+        <a className="navbar-brand" style={style.title} href="/">
+          FantasticFour
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-  )
-}
+    </nav>
+  );
+};
 
 const style = {
   title: {
-    fontWeight: 'bold',
-    FontFamily: 'Playwrite GB S, cursive', 
-    fontSize: '32px',
-  }
-}
+    fontWeight: "bold",
+    FontFamily: "Playwrite GB S, cursive",
+    fontSize: "32px",
+  },
+};
 
-export default Header
+export default Header;
