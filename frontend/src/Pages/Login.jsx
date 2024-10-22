d9b0f6d4674abf7e6fe33a5bb78250ee48bdf2c6
kshitijatram22 <atramkshitij6@gmail.com>
2024-10-22 22:05:02 +0530

    Added Log In pages

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const Login = () => {
+const LogIn = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div>
+      <div className="main">
+    <div className="innerMain">
+      <div className="leftSide">
+        <h1>The <span>Student</span> Log In</h1>
+        <p>Welcome Back,</p>
+        <p>Please enter your correct Log In Credentials.</p>
+      </div>
+      <div className="rightSide">
+        <h2>Log In</h2>
+        <form>
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          <button type="submit">Submit</button>
+          <a href="#">Forgot Password? Reset Now</a>
+          <p>or</p>
+          <p>Already have an account? <a href="./login.html">Sign In</a></p>
+
+        </form>
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
   )
 }
 
-export default Login
+export default LogIn
--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import "../Styles/SignUp.css";
 
-const Signup = () => {
+const SignUp = () => {
   return (
-    <div>Signup</div>
+    <div>
+      <div className="main">
+    <div className="innerMain">
+      <div className="leftSide">
+        <h1>The <span>Student</span> Sign Up</h1>
+        <p>Please enter your details to sign up and be</p>
+        <p>part of our great community.</p>
+      </div>
+      <div className="rightSide">
+        <h2>Sign Up</h2>
+        <form>
+          <input type="text" placeholder="Enter Full Name"/>
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          <input type="password" placeholder="Confirm Password"/>
+          <button type="submit">Submit</button>
+          <a href="#">Forgot Password? Reset Now</a>
+          <p>or</p>
+          <p>Already have an account? <a href="">Sign Up</a></p>
+
+        </form>
+      </div>
+    </div>
+    </div>
+    </div>
   )
 }
 
-export default Signup
+export default SignUp
