7387d8cd53234ce3a2f7c1ee7512f527b09444c0
doShant-dev <gdoshant@gmail.com>
2024-10-22 14:38:29 +0000

    Basic Header & Footer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
