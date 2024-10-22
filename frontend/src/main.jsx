70c4790d70aaf7ef98229242860b1d28e1131ece
devmulkalwar <devmulkalwar95@gmail.com>
2024-10-22 17:43:34 +0530

    minor changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 
 createRoot(document.getElementById('root')).render(
  
