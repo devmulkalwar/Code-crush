2e649a7752d6cf1d1012e5baa8843f1e43e4d0ea
devmulkalwar <devmulkalwar95@gmail.com>
2024-10-22 18:41:39 +0530

    routing and layout created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,31 @@
-// import './App.css'
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { Outlet } from "react-router-dom";
+import { ToastContainer, Bounce } from "react-toastify";  // Import Bounce transition
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div>
-      <button type="button" className="btn btn-primary">
-        First
-      </button>
-      <button type="button" className="btn btn-secondary">
-        Second
-      </button>
-      <button type="button" className="btn btn-success">
-        Third
-      </button>
-      <button type="button" className="btn btn-danger">
-        Fourth  
-      </button>
-      <button type="button" className="btn btn-warning">
-        Warning
-      </button>
-      <button type="button" className="btn btn-info">
-        Info
-      </button>
-      <button type="button" className="btn btn-light">
-        Light
-      </button>
-      <button type="button" className="btn btn-dark">
-        Dark
-      </button>
-
-      <button type="button" className="btn btn-link">
-        Link
-      </button>
+    <div className="d-flex flex-column min-vh-100"> 
+        <Header />
+      <main className="flex-grow-1">
+           <Outlet />
+      </main>
+      <Footer />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,61 @@
-// Import Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-// import './index.css'
+// pages import
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
+import Signup from "./Pages/Signup.jsx";
+import Login from "./Pages/Login.jsx";
+import Error from "./Pages/Error.jsx";
+import OTPPage from "./Pages/OTPPage.jsx";
+import ResetPassword from "./Pages/ResetPassword.jsx";
+import ForgotPassword from "./Pages/ForgotPassword.jsx";
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
-  
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "verify-otp",
+        element: <OTPPage />,
+      },
+      {
+        path: "forgot-password",
+        element: <ForgotPassword />,
+      },
+      {
+        path: "reset-password/:token",
+        element: <ResetPassword />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
